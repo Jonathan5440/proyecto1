b6fcb3a503da3cba92fe9795234e24a4cd8a15af
Jonathan5440 <jvaldezd4@miumg.edu.gt>
2025-02-14 21:22:29 -0600

    Generador de tablas del 4 y 6

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
+int main () {
+    cout << "Tablas del 4 y 6" << endl;
+    cout << "Tabla del 4:" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << "4 x" <<i<< "="<<4* i << endl;
+    }
+    cout << "tabla del 6"<< endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << "6 x"<<i<< "="<<6* i << endl;
+    }
 
+return 0;
 
-    return 0;
+}
